f808282aeae2059409a98305e5b14884e22ecedb
PipoDev <felipe.londono.zuluaga@gmail.com>
2024-09-24 20:50:56 -0500

    Agregado topappbar cuando es compact la pantalla

--- app/src/main/java/com/example/pipocity/ui/PipoCityHomeScreen.kt
+++ app/src/main/java/com/example/pipocity/ui/PipoCityHomeScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.material3.PermanentDrawerSheet
 import androidx.compose.material3.PermanentNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
